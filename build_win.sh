89c9f994eadf9409b2a4fa9b3f2f9e2a07f686e1
malottno <malottno@mail.uc.edu>
2018-05-30 08:36:24 -0400

    Update build script for no prompt

--- build_win.sh
+++ build_win.sh
@@ -32,104 +32,12 @@ rm -f ./hardware_layer.cpp
 rm -f ../build_core.sh
 
 echo The OpenPLC needs a driver to be able to control physical or virtual hardware.
-echo Please select the driver you would like to use:
-OPTIONS="Blank Modbus Fischertechnik RaspberryPi UniPi PiXtend PiXtend_2S Arduino ESP8266 Arduino+RaspberryPi Simulink "
-select opt in $OPTIONS; do
-	if [ "$opt" = "Blank" ]; then
-		cp ./hardware_layers/blank.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_normal.sh ../build_core.sh
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
-		exit
-	elif [ "$opt" = "Modbus" ]; then
-		cp ./hardware_layers/modbus_master.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_modbus_win.sh ../build_core.sh
-		echo [LIBMODBUS]
-		cd ..
-		cd libmodbus_src
-		./autogen.sh
-		./configure
-		make install
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
-		exit
-	elif [ "$opt" = "Fischertechnik" ]; then
-		cp ./hardware_layers/fischertechnik.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_rpi.sh ../build_core.sh
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
-		exit
-	elif [ "$opt" = "RaspberryPi" ]; then
-		cp ./hardware_layers/raspberrypi.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_rpi.sh ../build_core.sh
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
-		exit
-	elif [ "$opt" = "UniPi" ]; then
-		cp ./hardware_layers/unipi.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_rpi.sh ../build_core.sh
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
-		exit
-	elif [ "$opt" = "PiXtend" ]; then
-		cp ./hardware_layers/pixtend.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_rpi.sh ../build_core.sh
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
-		exit
-    elif [ "$opt" = "PiXtend_2S" ]; then
-		cp ./hardware_layers/pixtend2s.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_rpi.sh ../build_core.sh
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		exit
-	elif [ "$opt" = "Arduino" ]; then
-		cp ./hardware_layers/arduino.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_normal.sh ../build_core.sh
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
-		exit
-	elif [ "$opt" = "ESP8266" ]; then
-		cp ./hardware_layers/esp8266.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_normal.sh ../build_core.sh
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
-		exit
-	elif [ "$opt" = "Arduino+RaspberryPi" ]; then
-		cp ./hardware_layers/arduino.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_rpi.sh ../build_core.sh
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
-		exit
-	elif [ "$opt" = "Simulink" ]; then
-		cp ./hardware_layers/simulink.cpp ./hardware_layer.cpp
-		cp ./core_builders/build_normal.sh ../build_core.sh
-		echo [OPENPLC]
-		cd ..
-		./build_core.sh
-		echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
-		exit
-	else
-		#clear
-		echo bad option
-	fi
+cp ./hardware_layers/blank.cpp ./hardware_layer.cpp
+cp ./core_builders/build_normal.sh ../build_core.sh
+echo [OPENPLC]
+cd ..
+./build_core.sh
+echo 'export PATH=$PATH:"/cygdrive/c/Program Files/nodejs/"' >> ~/.bashrc
+
+
 done
